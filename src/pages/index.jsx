1e4f00500391fdeefc27157443cd28012558264d
Sizwe Mashele <77590275+seezmash@users.noreply.github.com>
2022-01-25 11:51:50 +0200

    completed net ninja tutorial

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,13 @@ import Nav from '../components/Nav'
 import NextHead from '../components/NextHead'
 import { useEffect } from 'react'
 import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
+} from 'firebase/auth'
+import {
   getFirestore,
   collection,
   getDocs,
@@ -22,6 +29,7 @@ const HomePage = () => {
 
   useEffect(() => {
     const db = getFirestore()
+    const auth = getAuth()
 
     // Get a single document
     const bookRef = doc(db, 'books', 'HpMhemds5BtPtHqfoqc8')
@@ -30,7 +38,7 @@ const HomePage = () => {
     //   console.log(doc.data(), doc.id)
     // })
 
-    onSnapshot(bookRef, (doc) => {
+    const unsubBook = onSnapshot(bookRef, (doc) => {
       console.log(doc.data(), doc.id)
     })
 
@@ -53,7 +61,7 @@ const HomePage = () => {
     //   })
 
     // Get initial data and on changes
-    onSnapshot(filteredColQuery, (snapshot) => {
+    const unsubBooksCol = onSnapshot(filteredColQuery, (snapshot) => {
       let books = []
       snapshot.docs.forEach((doc) => {
         books.push({ ...doc.data(), id: doc.id })
@@ -100,6 +108,67 @@ const HomePage = () => {
         }
       )
     })
+
+    // signing users up
+    const signupForm = document.querySelector('.signup')
+    signupForm.addEventListener('submit', (e) => {
+      e.preventDefault()
+
+      const email = signupForm.email.value
+      const password = signupForm.password.value
+
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((cred) => {
+          console.log('user created: ', cred.user)
+          signupForm.reset()
+        })
+        .catch((err) => {
+          console.log(err.message)
+        })
+    })
+
+    // logging in and out
+    const logoutButton = document.querySelector('.logout')
+    logoutButton.addEventListener('click', () => {
+      signOut(auth)
+        .then(() => {
+          console.log('the user signed out')
+        })
+        .catch((err) => {
+          console.log(err.message)
+        })
+    })
+
+    const loginForm = document.querySelector('.login')
+    loginForm.addEventListener('submit', (e) => {
+      e.preventDefault()
+
+      const email = loginForm.email.value
+      const password = loginForm.password.value
+
+      signInWithEmailAndPassword(auth, email, password)
+        .then((cred) => {
+          // console.log('user logged in: ', cred.user)
+          loginForm.reset()
+        })
+        .catch((err) => {
+          console.log(err.message)
+        })
+    })
+
+    // subscribing to auth changes
+    const unsubAuth = onAuthStateChanged(auth, (user) => {
+      console.log('user status changed: ', user)
+    })
+
+    // unsubscribing from changes (auth & db)
+    const unsubButton = document.querySelector('.unsub')
+    unsubButton.addEventListener('click', () => {
+      console.log('unsubscribing')
+      unsubAuth()
+      unsubBook()
+      unsubBooksCol()
+    })
   }, [])
 
   return (
@@ -107,31 +176,67 @@ const HomePage = () => {
       <NextHead title="Home | firebase 9 tutorial" />
       <Nav selectedPageIndex={0} />
 
-      <main className="max-w-7xl mx-auto px-6">
+      <main className="max-w-7xl mx-auto px-6 mb-40">
         <h2 className="mt-20 text-gray-800">Firebase Firestore</h2>
 
-        <form className="add mt-10">
+        {/* add book */}
+        <form className="add mt-10 p-4 bg-gray-100">
           <label htmlFor="title">Title:</label>
           <input type="text" name="title" required />
           <label htmlFor="author">Author:</label>
           <input type="text" name="author" required />
 
-          <button>add a new book</button>
+          <button className="ml-4 px-2 py-1 rounded bg-gray-200">
+            add a new book
+          </button>
         </form>
 
-        <form className="delete mt-5">
+        {/* delete book */}
+        <form className="delete mt-5 p-4 bg-gray-100">
           <label htmlFor="id">Document id:</label>
           <input type="text" name="id" required />
 
-          <button>delete a book</button>
+          <button className="ml-4 px-2 py-1 rounded bg-gray-200">
+            delete a book
+          </button>
         </form>
 
-        <form className="update mt-5">
+        {/* update book */}
+        <form className="update mt-5 p-4 bg-gray-100">
           <label htmlFor="id">Document id:</label>
           <input type="text" name="id" required />
 
-          <button>update a book</button>
+          <button className="ml-4 px-2 py-1 rounded bg-gray-200">
+            update a book
+          </button>
         </form>
+
+        {/* user sign up */}
+        <form className="signup mt-10 p-4 bg-gray-100">
+          <label htmlFor="email">email:</label>
+          <input type="email" name="email" />
+          <label htmlFor="password">password:</label>
+          <input type="password" name="password" />
+          <button className="ml-4 px-2 py-1 rounded bg-gray-200">signup</button>
+        </form>
+
+        {/* user sign up */}
+        <form className="login mt-10 p-4 bg-gray-100">
+          <label htmlFor="email">email:</label>
+          <input type="email" name="email" />
+          <label htmlFor="password">password:</label>
+          <input type="password" name="password" />
+          <button className="ml-4 px-2 py-1 rounded bg-gray-200">login</button>
+        </form>
+
+        <button className="logout mt-10 px-2 py-1 rounded bg-gray-200">
+          logout
+        </button>
+
+        <h2 className="mt-10">Unsubscribing</h2>
+        <button className="unsub mt-10 px-2 py-1 rounded bg-gray-200">
+          unsubscribe from db/auth changes
+        </button>
       </main>
     </>
   )
